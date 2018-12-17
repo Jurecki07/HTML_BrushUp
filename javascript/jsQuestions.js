840a681e0b6074fb4360aef53431a2111d4b9936
Tyler Jurecki <Jurecki07@gmail.com>
2018-12-17 13:27:02 -0500

    Working on variables

---
+++ javascript/jsQuestions.js
@@ -0,0 +1,39 @@
+let meal = 'Enchiladas';
+console.log(meal); // Output: Enchiladas
+meal = 'Burrito';
+console.log(meal); // Output: Burrito
+
+//--------------------------------------------------//
+
+const myName = 'Gilberto';
+console.log(myName); // Output: Gilberto
+
+//------------------------------//
+
+let x = 20;
+x -= 5; // Can be written as x = x - 5
+console.log(x); // Output: 15
+
+let y = 50;
+y *= 2; // Can be written as y = y * 2
+console.log(y); // Output: 100
+
+let z = 8;
+z /= 2; // Can be written as z = z / 2
+console.log(z); // Output: 4
+
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;
+// Increase the value by 5 
+powerLevel -= 100;
+// Decrease the value by 100
+multiplyMe *= 11;
+//multiply the value by 11
+quarterMe /= 4
+// divide by 4
