e54b9f687c27695f0c5339c9cb28a2e14dfaa220
Tyler Jurecki <Jurecki07@gmail.com>
2018-12-20 12:17:08 -0500

    Truthy vs Falsey

--- javascript/conditJS.js
+++ javascript/conditJS.js
@@ -24,5 +24,75 @@ if (hungerLevel > 7){
 } else {
   console.log('We can eat later!')
 }
+  // -----------------------------------------------------------
 
-  
+  // When we use the && operator, we are checking that two things are true:
+
+if (stopLight === 'green' && pedestrians === 0) {
+  console.log('Go!');
+} else {
+  console.log('Stop');
+}
+
+  // -----------------------------------------------------------
+
+
+// If we only care about either condition being true, we can use the || operator:
+
+if (day === 'Saturday' || day === 'Sunday') {
+  console.log('Enjoy the weekend!');
+} else {
+  console.log('Do some work.');
+}
+
+
+  // -----------------------------------------------------------
+
+
+// The ! not operator reverses, or negates, the value of a boolean:
+let excited = true;
+console.log(!excited); // Prints false
+
+let sleepy = false;
+console.log(!sleepy); // Prints true
+
+// --------------------------------------------
+
+let myVariable = 'I Exist!';
+if (myVariable) {
+   console.log(myVariable)
+} else {
+   console.log('The variable does not exist.')
+}
+
+
+// ------------------------------------------------
+
+let numberOfApples = 0;
+
+if (numberOfApples){
+   console.log('Let us eat apples!');
+} else {
+   console.log('No apples left!');
+}
+
+// Prints 'No apples left!'
+
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+// ------------------------------------------------
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
