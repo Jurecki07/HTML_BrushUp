93554cf9728e4b1b1af2a06b17bff54224fd1b63
Tyler Jurecki <Jurecki07@gmail.com>
2018-12-28 11:48:58 -0500

    Before meeting

--- javascript/functions/functions.js
+++ javascript/functions/functions.js
@@ -40,5 +40,45 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
   makeShoppingList();
   
   makeShoppingList('bob');
+
+  // Using the return statemnet within a function
+
+const numOfMonitors = monitorCount (5,4);
+
+
+function monitorCount(rows, columns){
+  return rows * columns
+}
+
+console.log (numOfMonitors)
+
+// Using Helper Functions or a function within a function
+
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  function costOfMonitors (rows, columns){
+    return monitorCount (rows,columns) * 200
+  }
+  
+  const totalCost = costOfMonitors (5,4);
+  
+  console.log(totalCost)
+  
+
+  // Using function expressions or a variable that is equal to a function ( anon function)
+
+  const plantNeedsWater = function(day) {
+    if(day === 'Wednesday'){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  plantNeedsWater('Tuesday');
+  
+  console.log(plantNeedsWater('Tuesday'));
   
   
