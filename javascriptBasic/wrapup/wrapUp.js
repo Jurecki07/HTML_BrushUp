5d4d580c9fa7276e8accfc4b8a2a1c74350f07ab
Tyler Jurecki <Jurecki07@gmail.com>
2019-01-08 18:58:59 -0500

    wrapping up the javascript basic

---
+++ javascriptBasic/wrapup/wrapUp.js
@@ -0,0 +1,7 @@
+
+
+// QUESTION 1
+// The most common minimum age to vote is 18. Write a function canIVote() that takes in a number,
+// representing the person's age, and returns the boolean true if they are 18 years old or older,
+// and the boolean false if they are not.
+
