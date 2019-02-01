929232a5481f2bd59be4088732b92e1b1711298e
Tyler Jurecki <Jurecki07@gmail.com>
2019-02-01 15:02:39 -0500

    Hourly update

---
+++ intermediateJavascript/examples.js
@@ -0,0 +1,21 @@
+// A block of code works like this function
+const logSkyColor = () => {
+    let color = 'blue';
+    console.log(color); // blue
+};
+// Observe the block in an if statement:
+if (dusk) {
+    let color = 'pink';
+    console.log(color); // pink
+};
+// Using Blocks and Scope
+
+const city = "New York City";
+
+function logCitySkyline (){
+    let skyscraper = 'Empire State Building'
+    return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+console.log(logCitySkyline());
+///////////////////////////////////////////////////////////////
+
