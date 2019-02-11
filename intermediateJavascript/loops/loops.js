72cdd59d51b829cf494d115044fdfea71933fb3b
Tyler Jurecki <Jurecki07@gmail.com>
2019-02-11 12:18:53 -0500

    mac

--- intermediateJavascript/loops/loops.js
+++ intermediateJavascript/loops/loops.js
@@ -41,3 +41,77 @@ for (let i = 0; i < vacationSpots.length; i++ ){
     console.log('I would love to visit ' + vacationSpots[i]);
 }
 
+/////////////////////////////////////////////////////
+
+// Nested Loops
+
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++) {
+    for (let j = 0; j < yourArray.length; j++) {
+        if (myArray[i] === yourArray[j]) {
+            console.log('Both loops have the number: ' + yourArray[j])
+        }
+    }
+};
+
+// Here is another example
+// off using a nested loop
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+
+
+// first loop goes through bobs followers
+for (let i = 0; i < bobsFollowers.length; i++) {
+    // with the length method we go through the entire array
+    for (let j = 0; j < tinasFollowers.length; j++) {
+        //Same for 2nd loop
+        // Conditonal to check whats equal and then we push
+        // from push bobs to mutal vaiable
+        if (bobsFollowers[i] === tinasFollowers[j]) {
+            mutualFollowers.push(bobsFollowers[i]);
+        }
+    }
+};
+//////////////////////////////////////////////
+
+// Using the while loop
+// lets convert from for to while
+
+// A for loop that prints 1, 2, and 3
+for (let counterOne = 1; counterOne < 4; counterOne++){
+    console.log(counterOne);
+}
+
+// A while loop that prints 1, 2, and 3
+
+//CounterTwo variable declared before the loop, it is global.
+let counterTwo = 1;
+//1st stopping condontion is countertwo is greater then 4
+while (counterTwo < 4) {
+    console.log(counterTwo);
+    counterTwo++;
+}
+
+/////////////////////////////////
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+// Empty variable
+let currentCard;
+
+// Starting loop runs the loop until currentCard is not equal to spade
+while ( currentCard != 'spade') {
+    // using math.random to index the cards array
+    currentCard = cards[Math.floor(Math.random() * 4)];
+    console.log(currentCard);
+}
+
+
+
+
+
+
